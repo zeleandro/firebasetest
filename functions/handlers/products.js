67c0433f4fc82da224ddf3762de2a4dd185a919e
Leandro <pallud_leandro@hotmail.com>
2020-10-20 08:55:37 -0300

    upload images

---
+++ functions/handlers/products.js
@@ -0,0 +1,122 @@
+const { admin, db } = require('../util/admin');
+const config = require('../util/config');
+// const { uuid } = require("uuidv4");
+
+exports.getAllProducts = (req, res) => {
+    db
+        .collection('productos')
+        .orderBy('title')
+        .get()
+        .then(data => {
+            let products = [];
+            data.forEach(doc => {
+                products.push({
+                    productId: doc.id,
+                    ...doc.data()
+                });
+            });
+            return res.json(products);
+        })
+        .catch((err) => console.log(err));
+};
+
+exports.postOneProduct = (req, res) => {
+    const newProduct = {
+        title: req.body.title,
+        price: req.body.price,
+        category: req.body.category,
+        description: req.body.description,
+        createdAt: new Date().toISOString()
+    };
+
+    db
+        .collection('productos')
+        .add(newProduct)
+        .then(doc => {
+            res.json({ message: `document ${doc.id} created successfully` });
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'something went wrong' });
+            console.error(err);
+        })
+};
+
+// Fetch one product
+exports.getProduct = (req, res) => {
+    let productData = {};
+    db.doc(`/productos/${req.params.productId}`)
+      .get()
+      .then((doc) => {
+        if (!doc.exists) {
+          return res.status(404).json({ error: 'Product not found' });
+        }
+        productData = doc.data();
+        productData.productId = doc.id;
+      })
+      .then((data) => {
+        return res.json(productData);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json({ error: err.code });
+      });
+  };
+
+    // Upload image for product
+  exports.uploadImage = (req, res) => {
+    const BusBoy = require("busboy");
+    const path = require("path");
+    const os = require("os");
+    const fs = require("fs");
+
+    const busboy = new BusBoy({ headers: req.headers });
+
+    let imageToBeUploaded = {};
+    let imageFileName;
+    // String for image token
+    // let generatedToken = uuid();
+
+    busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+      console.log(fieldname, file, filename, encoding, mimetype);
+      if (mimetype !== "image/jpeg" && mimetype !== "image/png") {
+        return res.status(400).json({ error: "Wrong file type submitted" });
+      }
+      // my.image.png => ['my', 'image', 'png']
+      const imageExtension = filename.split(".")[filename.split(".").length - 1];
+      // 32756238461724837.png
+      imageFileName = `${Math.round(
+        Math.random() * 1000000000000
+      ).toString()}.${imageExtension}`;
+      const filepath = path.join(os.tmpdir(), imageFileName);
+      imageToBeUploaded = { filepath, mimetype };
+      file.pipe(fs.createWriteStream(filepath));
+    });
+    busboy.on("finish", () => {
+      admin
+        .storage()
+        .bucket()
+        .upload(imageToBeUploaded.filepath, {
+          resumable: false,
+          metadata: {
+            metadata: {
+              contentType: imageToBeUploaded.mimetype,
+              //Generate token to be appended to imageUrl
+            //   firebaseStorageDownloadTokens: generatedToken,
+            },
+          },
+        })
+        .then(() => {
+          // Append token to url
+          const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
+          return db.doc(`/productos/${req.params.productId}`).update({ imageUrl });
+        })
+        .then(() => {
+          return res.json({ message: "image uploaded successfully" });
+        })
+        .catch((err) => {
+          console.error(err);
+          return res.status(500).json({ error: "something went wrong" });
+        });
+    });
+    busboy.end(req.rawBody);
+  };
---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyDLhKU910PPmRNja-tk5kR70XvZ7nq-lqM",
+    authDomain: "dglimpieza-75f83.firebaseapp.com",
+    databaseURL: "https://dglimpieza-75f83.firebaseio.com",
+    projectId: "dglimpieza-75f83",
+    storageBucket: "dglimpieza-75f83.appspot.com",
+    messagingSenderId: "283530166516",
+    appId: "1:283530166516:web:dd03b58bf438543cbf9ea1"
+  };
